71512865940b03ed3528f4046a1fc74e41c34638
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-16 18:24:46 +0100

    saved routes

--- router/admin/adminDashboardRoutes.ts
+++ router/admin/adminDashboardRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { createBills } from "../../controller/admin/adminBills";
 import { sendMessage } from "../../controller/admin/adminMsgController";
 
 const adminDashboard = Router();
 
 adminDashboard.post("/admintoclient/:adminId/:userId", sendMessage);
+
+//bills
+adminDashboard.post("/bills/:clientId/:adminId", createBills);
 
 export default adminDashboard;
