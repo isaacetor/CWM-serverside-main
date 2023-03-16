946a4d9d10d31d420bdd44961fa9165f8d0ed878
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-16 12:47:04 +0100

    working on validation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from "express";
 import cors from "cors";
+import { errorHandler } from "../middleware/error";
 
 const appConfig = (app: Application) => {
   app.use(express.json()).use(cors());
   //routes
 
   //error handler : should be the last imported middleware in your application
+  app.use(errorHandler);
 };
 
 export default appConfig;
